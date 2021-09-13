eb0ecb7f7864ea75dccb88bd3b759f37f7dd8b31
MGicas <89281701+MGicas@users.noreply.github.com>
2021-09-13 17:47:29 -0500

    prueba
    esto es para mirar si funciona

--- src/Parcial20212/app/appParcial.java
+++ src/Parcial20212/app/appParcial.java
@@ -3,5 +3,6 @@ package Parcial20212.app;
 public class appParcial {
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("xdddd");
     }
 }
